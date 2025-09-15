cff3806b240f07bea4a01980f35f3ca0b70e8494
jky-20 <jaiy.it.16@nsit.net.in>
2025-09-15 15:36:52 +0530

    ClassRoom Codes

---
+++ Lec_46_September_15_Graphs/DSU.java
@@ -0,0 +1,33 @@
+package Lec_46_September_15_Graphs;
+
+public class DSU {
+
+    static int find(int i, int[] parent){
+        if(i == parent[i]){
+            return i;
+        }
+        return find(parent[i], parent);
+    }
+    static void union(int i, int j, int[] parent){
+
+        int irep = find(i, parent);
+        int jrep = find(j, parent);
+
+        if(irep == jrep){
+            return;
+        }
+        parent[irep] = jrep;
+    }
+    public static void main(String[] args) {
+        
+        int n = 5;  // number od nodes
+        int numEdges = 10;  // independent of no. of nodes
+
+        int[][] edges = new int[numEdges][2];
+
+        for(int[] edge : edges){
+            int i = edge[0], j=edge[1];
+            union(i, j, parent);
+        }
+    }
+}
